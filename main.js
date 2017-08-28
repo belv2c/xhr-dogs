cc31004975f99e3a8497c034bf03da73b2d2abbd
Brooke Lewis <belv2c@gmail.com>
2017-08-28 18:48:08 -0500

    added json

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("DOGS");
+
+function executeThisCodeAfterFileLoads() {
+	var data = JSON.parse(this.responseText);
+	console.log("dogs", data.dogs);
+}
+
+function executeThisCodeIfFileErrors() {
+	console.log("nope");
+}
+
+var myRequest = new XMLHttpRequest();
+myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
+myRequest.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest.open("GET", "dogs.json");
+myRequest.send();
