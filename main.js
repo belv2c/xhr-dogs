9c245b54892e665a5218c6b7a5cdfa65238ae121
Brooke Lewis <belv2c@gmail.com>
2017-08-28 21:04:41 -0500

    exercise inclass

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 console.log("DOGS");
 
 function executeThisCodeAfterFileLoads() {
-	var data = JSON.parse(this.responseText);
-	console.log("dogs", data.dogs);
-}
+	var dogsData = JSON.parse(this.responseText).dogs;
+/*	console.log("dogs", dogsData);*/
+	getBreedz(dogsData);
+}	
 
-function executeThisCodeAfterFileLoads2() {
-	var data = JSON.parse(this.responseText);
-	console.log("breeds", data.breeds);
-}
 
 function executeThisCodeIfFileErrors() {
 	console.log("nope");
@@ -20,8 +17,53 @@ myRequest.addEventListener("error", executeThisCodeIfFileErrors);
 myRequest.open("GET", "dogs.json");
 myRequest.send();
 
-var myRequest2 = new XMLHttpRequest();
-myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
-myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
-myRequest2.open("GET", "breeds.json");
-myRequest2.send();
+function getBreedz(dogz){
+	/*console.log("dogs array inside getBreedz", dogz);*/
+	var myRequest2 = new XMLHttpRequest();
+	myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+	myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
+	myRequest2.open("GET", "breeds.json");
+	myRequest2.send();
+
+	function executeThisCodeAfterFileLoads2() {
+		var breedsData = JSON.parse(this.responseText).breeds;
+		/*console.log("breeds", breedsData);
+		console.log("dogs next to breeds", dogz);*/
+		combinedArray(dogz, breedsData);
+	}
+}
+
+function combinedArray (dogsArray, breedsArray){
+	/*console.log("dogsArray", dogsArray);
+	console.log("breedsArray", breedsArray);*/
+	// loop through dogs and look at breed_id
+	dogsArray.forEach(function(dog){
+		var currentBreedId = dog["breed-id"];
+		/*console.log("dog breed_id", currentBreedId);*/
+		breedsArray.forEach(function(breed){
+			if(currentBreedId === breed.id){
+			/*console.log("one breed", breed);*/
+			dog["dogBreed"] = breed.name;
+			dog["basePrice"] = breed["base-price"];
+		    dog["description"] = breed.description;
+		    dog["finalPrice"] = dog.basePrice + dog["add-on-price"];
+		    }
+		});
+		
+	});
+	// loop through breeds and find matching breed_id
+	// make final price
+	console.log("all the dogs", dogsArray);
+	domString(dogsArray);
+}
+
+function domString(dogs){
+	// usual stuff - fat for loop
+	var reallyLongDomString = "";
+	writeToDom(reallyLongDomString);
+}
+
+function writeToDom(strang){
+	// put in the DOM;
+
+}
