4304dcfa957fe1b6b04baaa32edf1dda2f882552
Brooke Lewis <belv2c@gmail.com>
2017-08-28 19:09:48 -0500

    json prints to console

--- main.js
+++ main.js
@@ -5,6 +5,11 @@ function executeThisCodeAfterFileLoads() {
 	console.log("dogs", data.dogs);
 }
 
+function executeThisCodeAfterFileLoads2() {
+	var data = JSON.parse(this.responseText);
+	console.log("breeds", data.breeds);
+}
+
 function executeThisCodeIfFileErrors() {
 	console.log("nope");
 }
@@ -14,3 +19,9 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoads);
 myRequest.addEventListener("error", executeThisCodeIfFileErrors);
 myRequest.open("GET", "dogs.json");
 myRequest.send();
+
+var myRequest2 = new XMLHttpRequest();
+myRequest2.addEventListener("load", executeThisCodeAfterFileLoads2);
+myRequest2.addEventListener("error", executeThisCodeIfFileErrors);
+myRequest2.open("GET", "breeds.json");
+myRequest2.send();
